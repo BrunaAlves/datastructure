db55be20c4bc7a6ef034d4eef1ecbf7386eee1b6
BrunaAlves <brunahalves94@gmail.com>
2023-01-25 20:57:43 +0100

    Add Contains and Insert BinarySearchTree

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -2,7 +2,7 @@ package binarysearchtree;
 
 public class BinarySearchTree {
 
-    private Node root;
+    public Node root;
 
     public static class Node {
         public int value;
@@ -53,5 +53,37 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    private boolean rContains(Node currentNode, int value){
+        if(currentNode == null) return false;
+        if(currentNode.value == value) return true;
+
+        if(value < currentNode.value){
+            return rContains(currentNode.left, value);
+        }else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value){
+        if(currentNode == null) return new Node(value);
+
+        if(value < currentNode.value){
+            currentNode.left = rInsert(currentNode.left, value);
+        }else if(value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value){
+        if(root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
 
 }
